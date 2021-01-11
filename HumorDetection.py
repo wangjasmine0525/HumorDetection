0449f8ab7bc89fda64316297f09a3062b5503278
Jasmine Wang <wangjasmine0525@berkeley.edu>
2021-01-10 17:44:18 -0800

    committing before pulling Marlon's update

--- HumorDetection.py
+++ HumorDetection.py
@@ -11,17 +11,16 @@ from sklearn.svm import SVC, LinearSVC
 filepath = '/Users/JasmineW/Desktop/Projects/HumorDetection/data/master_dataset.csv'
 data = pd.read_csv(filepath)
 
+''' Transform features using TfidfVectorizer '''
 from sklearn.model_selection import train_test_split
 
 train_x_ngram, test_x_ngram, train_y_ngram, test_y_ngram = train_test_split(data.Line, data.Funny, test_size = 0.2, random_state = 2)
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 
-
 tfidf_ngram = TfidfVectorizer(ngram_range = (1, 2))
 train_1_ngram = tfidf_ngram.fit_transform(train_x_ngram)
 test_1_ngram = tfidf_ngram.transform(test_x_ngram)
-
 
 train_arr_ngram = train_1_ngram.toarray()
 test_arr_ngram = test_1_ngram.toarray()
@@ -32,16 +31,11 @@ svc_ngram = LinearSVC()
 svc_ngram.fit(train_arr_ngram, train_y_ngram)
 svc_ngram_predicted = svc_ngram.predict(test_arr_ngram)
 svc_accuracy_ngram = accuracy_score(test_y_ngram, svc_ngram_predicted) * 100
-#print(svc_accuracy_ngram)
-
-
-
 
 ''' Returns whether or not a user inputted string is funny. '''
 def is_funny(str):
     test = tfidf_ngram.transform([str])
     predicted_val = svc_ngram.predict(test.toarray())
     return predicted_val
-
 
 print(is_funny("I like to eat"))
