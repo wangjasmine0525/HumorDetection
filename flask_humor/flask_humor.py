866c5228c2223d4a2f10d8e8af60679f616ace89
Marlon Fu <marlonfu@berkeley.edu>
2021-01-12 15:45:01 -0800

    included randomly generated responses

--- flask_humor/flask_humor.py
+++ flask_humor/flask_humor.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import pickle
+import numpy as np
 
 app = Flask(__name__)
 svc = pickle.load(open("HumorDetection.pkl", "rb"))
@@ -7,11 +8,22 @@ tfidf = pickle.load(open("tfidf.pkl", "rb"))
 
 @app.route("/", methods=['POST', 'GET'])
 def home():
+    laughs = ["You're quite the jokester!",
+              "You should be a comedian!",
+              "That's a good one!"]
+    lames = ["You're lame.",
+             "Nice try.",
+             "Seems like you're the only one laughing."]
+
     if request.method =='POST':
-        joke = request.form['content'] 
+        rand_num = np.random.randint(3)
+        joke = request.form['content']
         transformed = tfidf.transform([joke])
         funny = svc.predict(transformed.toarray())
-        return render_template('results.html', joke=joke, funny=funny)
+        if funny == 1:
+            return render_template('results.html', joke=joke, funny=funny, message=laughs[rand_num])
+        elif funny == 0:
+            return render_template('results.html', joke=joke, funny=funny, message=lames[rand_num])
     else:
         return render_template('home.html')
 
